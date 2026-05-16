310f4d82b901935026681cbf5e6865979b5fdec3
Admin <Admin@D21209>
2026-05-16 09:19:53 +0700

    Add project files.

---
+++ CMS.DATA/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using CMS.DATA.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.DATA.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string FullName { get; set; }
+        public string Role { get; set; } // Quản trị viên hoặc Biên tập viên
+    }
+}
